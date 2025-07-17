f95061a8f9003c99f89724ff905302278413e9fb
kelvyn2012 <kudufo@gmail.com>
2025-07-17 21:19:46 +0100

    updated

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth import login
 from .views import (
     list_books,
     LibraryDetailView,
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from .models import Book
 from .models import Library
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import FormView
@@ -28,9 +29,9 @@ class CustomLogoutView(LogoutView):
 
 
 class RegisterView(FormView):
-    template_name = 'relationship_app/register.html'
+    template_name = "relationship_app/register.html"
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy("login")
 
     def form_valid(self, form):
         form.save()
