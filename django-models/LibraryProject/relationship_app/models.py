6ab6c8db61596738d24e9dbe723d2d4b9494ce59
kelvyn2012 <kudufo@gmail.com>
2025-07-17 15:02:42 +0100

    updated

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -11,10 +11,12 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.PositiveIntegerField()
+    author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.name} by {self.author.name}"
+        return f"{self.title} by {self.author.name} {self.publication_year}"
 
 
 class Library(models.Model):
