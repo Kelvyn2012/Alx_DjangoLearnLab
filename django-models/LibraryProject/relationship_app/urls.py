685f49e1ab0ec4a482b88cddba03781bc377e3e3
kelvyn2012 <kudufo@gmail.com>
2025-07-18 18:47:47 +0100

    updated

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from . import views  # needed for views.register
+from . import views
 
 urlpatterns = [
     path("books/", views.list_books, name="list_books"),
@@ -16,4 +16,7 @@ urlpatterns = [
         name="logout",
     ),
     path("register/", views.register, name="register"),
+    path("admin-view/", views.admin_view, name="admin_view"),
+    path("librarian-view/", views.librarian_view, name="librarian_view"),
+    path("member-view/", views.member_view, name="member_view"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,11 @@
 from .models import Book
-from .models import Library
+from .models import Library, UserProfile
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LogoutView, LoginView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test, login_required
 
 
 def list_books(request):
@@ -36,3 +37,29 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
+
+
+def check_role(role):
+    def inner(user):
+        return (
+            user.is_authenticated
+            and hasattr(user, "userprofile")
+            and user.userprofile.role == role
+        )
+
+    return inner
+
+
+@user_passes_test(check_role("Admin"))
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(check_role("Librarian"))
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(check_role("Member"))
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
