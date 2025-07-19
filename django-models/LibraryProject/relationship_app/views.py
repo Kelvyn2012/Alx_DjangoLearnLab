186b5121f36bb025b42ba1e52f94906b304d82e0
kelvyn2012 <kudufo@gmail.com>
2025-07-19 08:35:53 +0100

    updated

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,14 @@
 from .models import Book, Library, UserProfile
 from django.contrib.auth import login
-from django.contrib.auth.decorators import (
-    permission_required,
-    user_passes_test,
-    login_required,
-)
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LogoutView, LoginView
 from django.contrib.auth.forms import UserCreationForm
 from .forms import BookForm
 from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required
 
 
 # ----------------------------
