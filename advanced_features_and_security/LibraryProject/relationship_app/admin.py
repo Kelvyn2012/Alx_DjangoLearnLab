268793b41e2f23ef51c55e494eb1ca18669d4c9f
kelvyn2012 <kudufo@gmail.com>
2025-07-24 07:09:08 +0100

    updated

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Author, Book, Library, Librarian, UserProfile
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
 admin.site.register(Author)
@@ -7,3 +10,13 @@ admin.site.register(Book)
 admin.site.register(Library)
 admin.site.register(Librarian)
 admin.site.register(UserProfile)
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (_("Additional Info"), {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (_("Additional Info"), {"fields": ("date_of_birth", "profile_photo")}),
+    )
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,24 +1,58 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser, BaseUserManager
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email field is required")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        if not extra_fields.get("is_staff"):
+            raise ValueError("Superuser must have is_staff=True.")
+        if not extra_fields.get("is_superuser"):
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(
+        upload_to="profile_photos/", null=True, blank=True
+    )
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ("Admin", "Admin"),
         ("Librarian", "Librarian"),
         ("Member", "Member"),
     ]
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     role = models.CharField(max_length=100, choices=ROLE_CHOICES)
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=CustomUser)
 def create_of_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance, role="Member")
@@ -59,6 +93,6 @@ class Library(models.Model):
 
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
-    liabrary = models.OneToOneField(
+    library = models.OneToOneField(
         Library, on_delete=models.CASCADE, related_name="librarians"
     )
