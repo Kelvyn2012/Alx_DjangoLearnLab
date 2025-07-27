9289400399cd13569e90f7824bc55d84b9aacc96
kelvyn2012 <kudufo@gmail.com>
2025-07-27 08:07:05 +0100

    updated

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.csrf import csrf_protect
 from .models import Book
-from .forms import BookForm
+from .forms import BookForm, ExampleForm
 
 
 @permission_required("bookshelf.can_view", raise_exception=True)
@@ -46,3 +46,15 @@ def book_delete(request, pk):
         book.delete()
         return redirect("book_list")
     return render(request, "confirm_delete.html", {"book": book})
+
+
+@csrf_protect
+def example_form_view(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # process form.cleaned_data
+            return redirect("success_page")
+    else:
+        form = ExampleForm()
+    return render(request, "form_example.html", {"form": form})
