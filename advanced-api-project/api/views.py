7423d38f952e58c341908d46273df16d063aa4b6
kelvyn2012 <kudufo@gmail.com>
2025-08-07 20:26:39 +0100

    updated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,8 +3,8 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import filters
+from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 
