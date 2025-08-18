8b406c8696afb917c736f233016807dcaab38929
kelvyn2012 <kudufo@gmail.com>
2025-08-18 18:53:31 +0100

    updated my password security by using create_user instead of create

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,19 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)  # ✅ accept password safely
 
     class Meta:
         model = User
         fields = ("id", "username", "email", "password", "bio", "profile_picture")
 
     def create(self, validated_data):
-        # create user
+        # ✅ use create_user so password is hashed
         user = User.objects.create_user(
             username=validated_data["username"],
             email=validated_data.get("email"),
@@ -21,8 +20,6 @@ class RegisterSerializer(serializers.ModelSerializer):
             bio=validated_data.get("bio", ""),
             profile_picture=validated_data.get("profile_picture", None),
         )
-        # generate token for user
-        Token.objects.create(user=user)
         return user
 
 
