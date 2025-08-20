1f6756e75c2974dc12f70f47151fb70cf12e997d
kelvyn2012 <kudufo@gmail.com>
2025-08-20 21:14:08 +0100

    added feeds and also updated my account models to include followers

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -7,9 +7,21 @@ class User(AbstractUser):
     profile_picture = models.ImageField(
         upload_to="profile_pics/", blank=True, null=True
     )
-    followers = models.ManyToManyField(
-        "self", symmetrical=False, related_name="following", blank=True
+    following = models.ManyToManyField(
+        "self", symmetrical=False, related_name="followers", blank=True
     )
 
     def __str__(self):
         return self.username
+
+    def follow(self, user):
+        """Follow another user"""
+        self.following.add(user)
+
+    def unfollow(self, user):
+        """Unfollow a user"""
+        self.following.remove(user)
+
+    def is_following(self, user):
+        """Check if following a user"""
+        return self.following.filter(id=user.id).exists()
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import authenticate, get_user_model
-from rest_framework import generics, status
+from rest_framework import generics, status, permissions, views
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from .serializers import RegisterSerializer, UserSerializer
+from django.shortcuts import get_object_or_404
 
 User = get_user_model()
 
@@ -44,4 +45,32 @@ class LoginView(APIView):
             return Response({"token": token.key}, status=status.HTTP_200_OK)
         return Response(
             {"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class FollowUserView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        target_user = get_object_or_404(User, id=user_id)
+        if request.user == target_user:
+            return Response({"error": "You cannot follow yourself."}, status=400)
+
+        request.user.follow(target_user)
+        return Response(
+            {"message": f"You are now following {target_user.username}"}, status=200
+        )
+
+
+class UnfollowUserView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        target_user = get_object_or_404(User, id=user_id)
+        if request.user == target_user:
+            return Response({"error": "You cannot unfollow yourself."}, status=400)
+
+        request.user.unfollow(target_user)
+        return Response(
+            {"message": f"You have unfollowed {target_user.username}"}, status=200
         )
